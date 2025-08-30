e83ebf53e81b76161a17634c9de80fdbbf035caf
Poxman100 <65652333+Poxman100@users.noreply.github.com>
2025-08-30 11:08:42 +0800

    Added list

--- src/main/java/Pecky.java
+++ src/main/java/Pecky.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Pecky {
@@ -6,6 +7,9 @@ public class Pecky {
             "What can I do for you?";
 
     private static String BYE = "Bye. Hope to see you again soon!";
+
+    private static ArrayList<String> LIST = new ArrayList<String>(1);
+    private static StringBuilder LIST_STRING = new StringBuilder();
 
     private static void printOutput(String s) {
         System.out.println("____________________________________________________________");
@@ -24,7 +28,30 @@ public class Pecky {
             return 1;
         }
 
-        printOutput(s);
+        if (s.equals("list")) {
+            if (LIST.isEmpty()) {
+                printOutput("");
+                return 0;
+            }
+            LIST_STRING = new StringBuilder();
+            for (int i=0; i<LIST.size()-1; i++) {
+                LIST_STRING.append(i+1);
+                LIST_STRING.append(". ");
+                LIST_STRING.append(LIST.get(i));
+                LIST_STRING.append("\n");
+            }
+
+            LIST_STRING.append(LIST.size());
+            LIST_STRING.append(". ");
+            LIST_STRING.append(LIST.get(LIST.size()-1));
+
+            printOutput(LIST_STRING.toString());
+
+            return 0;
+        }
+
+        LIST.add(s);
+        printOutput("added: " + s);
         return 0;
     }
 
