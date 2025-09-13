606642b0bb1a4bc31bdae44c41f7dbe91cead8d4
Poxman100 <65652333+Poxman100@users.noreply.github.com>
2025-09-13 17:57:22 +0800

    Refactor the command function The method command(String s) has too many LOC. This causes issues with code readability. Let’s * use switch statements instead of the if-else statements. * handle each case by calling its own separate function. This way, the command function is reduced to 30 LOC which is more acceptable.

--- src/main/java/Pecky.java
+++ src/main/java/Pecky.java
@@ -37,103 +37,120 @@ public class Pecky {
         printOutput("Got it. I've added this task: \n  " + t + "\nNow you have " + LIST_SIZE + " tasks in the list.");
     }
 
-    private static void deleteTask(int t) {
-        Task taskRemoved = LIST.get(t-1);
-        LIST.remove(t-1);
+    private static void updateTaskFile() {
+
+    }
+
+    private static void bye() {
+        printOutput(BYE);
+    }
+
+    private static void list() {
+        if (LIST_SIZE == 0) {
+            printOutput("");
+            return;
+        }
+
+        LIST_STRING = new StringBuilder();
+        LIST_STRING.append("Here are the tasks in your list:\n");
+        for (int i=0; i<LIST_SIZE-1; i++) {
+            LIST_STRING.append(i+1);
+            LIST_STRING.append(". ");
+            LIST_STRING.append(LIST.get(i).toString());
+            LIST_STRING.append("\n");
+        }
+
+        LIST_STRING.append(LIST_SIZE);
+        LIST_STRING.append(". ");
+        LIST_STRING.append(LIST.get(LIST_SIZE-1).toString());
+
+        printOutput(LIST_STRING.toString());
+    }
+
+    private static void mark(String s) {
+        int index = Integer.parseInt(s.substring(5));
+        LIST.get(index-1).markDone();
+        printOutput("Nice! I've marked this task as done:\n  " + LIST.get(index-1).toString());
+    }
+
+    private static void unmark(String s) {
+        int index = Integer.parseInt(s.substring(7));
+        LIST.get(index-1).markNotDone();
+        printOutput("OK, I've marked this task as not done yet:\n  " + LIST.get(index-1).toString());
+    }
+
+    private static void todo(String s) {
+        String[] args = s.split(" ");
+        if (args.length == 1) {
+            printOutput("OOPS!!! The description of a todo cannot be empty.");
+            return;
+        }
+        String description = s.substring(5);
+        addTask(new Todo(description));
+    }
+
+    private static void deadline(String s) {
+        s = s.substring(9).trim();
+        String[] parts = s.split(" /by ");
+        String description = parts[0].trim();
+        String by = parts[1].trim();
+        addTask(new Deadline(description, by));
+    }
+
+    private static void event(String s) {
+        s = s.substring(6).trim();
+        String[] parts = s.split(" /from ");
+        String description = parts[0].trim();
+        String[] timeParts = parts[1].split(" /to ");
+        String from = timeParts[0];
+        String to = timeParts[1];
+        addTask(new Event(description, from, to));
+    }
+
+    private static void delete(String s) {
+        int index = Integer.parseInt(s.substring(7));
+        Task taskRemoved = LIST.get(index-1);
+        LIST.remove(index-1);
         LIST_SIZE -= 1;
         printOutput("     Noted. I've removed this task:\n  " + taskRemoved + "\nNow you have " + LIST_SIZE + " tasks in the list.");
     }
 
-    private static void updateTaskFile() {
-        
+    private static void unknownCommand() {
+        printOutput("OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
 
     private static int command(String s) {
         String[] args = s.split(" ");
 
-        if (s.equals("bye")) {
-            printOutput(BYE);
-            return 1;
-        }
-
-        if (s.equals("list")) {
-            if (LIST_SIZE == 0) {
-                printOutput("");
+        switch (args[0]) {
+            case "bye":
+                bye();
+                return 1;
+            case "list":
+                list();
                 return 0;
-            }
-
-            LIST_STRING = new StringBuilder();
-            LIST_STRING.append("Here are the tasks in your list:\n");
-            for (int i=0; i<LIST_SIZE-1; i++) {
-                LIST_STRING.append(i+1);
-                LIST_STRING.append(". ");
-                LIST_STRING.append(LIST.get(i).toString());
-                LIST_STRING.append("\n");
-            }
-
-            LIST_STRING.append(LIST_SIZE);
-            LIST_STRING.append(". ");
-            LIST_STRING.append(LIST.get(LIST_SIZE-1).toString());
-
-            printOutput(LIST_STRING.toString());
-
-            return 0;
-        }
-
-        if (args[0].equals("mark")) {
-            int index = Integer.parseInt(s.substring(5));
-            LIST.get(index-1).markDone();
-            printOutput("Nice! I've marked this task as done:\n  " + LIST.get(index-1).toString());
-            return 0;
-        }
-
-        if (args[0].equals("unmark")) {
-            int index = Integer.parseInt(s.substring(7));
-            LIST.get(index-1).markNotDone();
-            printOutput("OK, I've marked this task as not done yet:\n  " + LIST.get(index-1).toString());
-            return 0;
-        }
-
-        if (args[0].equals("todo")) {
-            if (args.length == 1) {
-                printOutput("OOPS!!! The description of a todo cannot be empty.");
+            case "mark":
+                mark(s);
                 return 0;
-            }
-            addTask(new Todo(s.substring(5)));
-            return 0;
+            case "unmark":
+                unmark(s);
+                return 0;
+            case "todo":
+                todo(s);
+                return 0;
+            case "deadline":
+                deadline(s);
+                return 0;
+            case "event":
+                event(s);
+                return 0;
+            case "delete":
+                delete(s);
+                return 0;
+            default:
+                unknownCommand();
+                return 0;
         }
-
-        if (s.length() >= 6 && s.substring(0, 5).equals("todo ")) {
-            addTask(new Todo(s));
-        }
-
-        if (args[0].equals("deadline")) {
-            s = s.substring(9).trim();
-            String[] parts = s.split(" /by ");
-            String description = parts[0].trim();
-            String by = parts[1].trim();
-            addTask(new Deadline(description, by));
-            return 0;
-        }
-
-
-        if (args[0].equals("event")) {
-            s = s.substring(6).trim();
-            String[] parts = s.split(" /from ");
-            String description = parts[0].trim();
-            String[] timeParts = parts[1].split(" /to ");
-            addTask(new Event(description, timeParts[0].trim(), timeParts[1].trim()));
-            return 0;
-        }
-
-        if (args[0].equals("delete")) {
-            int index = Integer.parseInt(s.substring(7));
-            deleteTask(index);
-            return 0;
-        }
-
-        printOutput("OOPS!!! I'm sorry, but I don't know what that means :-(");
-        return 0;
     }
 
     private static void initialize() {
@@ -154,7 +171,7 @@ public class Pecky {
 
     public static void main(String[] args) {
         initialize();
-
+        
         printOutput(HELLO);
 
         while (command(takeInput()) == 0);
