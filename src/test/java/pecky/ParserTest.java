0ada25a8288bca58b4d6079406987b02ade21ae4
Poxman100 <65652333+Poxman100@users.noreply.github.com>
2025-09-14 02:13:44 +0800

    Add JUnit tests for two methods from the Task and Parser classes

---
+++ src/test/java/pecky/ParserTest.java
@@ -0,0 +1,44 @@
+package pecky;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class ParserTest {
+
+    @Test
+    public void parse_validInput1() {
+        assertDoesNotThrow(() -> Parser.parse("list"));
+    }
+
+    @Test
+    public void parse_invalidInput1() {
+        assertEquals(0, Parser.parse("unknown"));
+    }
+
+    @Test
+    public void parse_invalidInput2() {
+        assertEquals(0,  Parser.parse("asdfmuoiawef 0awef"));
+    }
+
+    @Test
+    public void parse_invalidInput3() {
+        assertEquals(0,  Parser.parse("mark asdf"));
+    }
+
+    @Test
+    public void parse_invalidInput4() {
+        assertEquals(0,  Parser.parse("todo"));
+    }
+
+    @Test
+    public void parse_invalidInput5() {
+        assertEquals(0,  Parser.parse("date"));
+    }
+
+    @Test
+    public void parse_invalidInput6() {
+        assertEquals(0,  Parser.parse("date 00000000"));
+    }
+}
