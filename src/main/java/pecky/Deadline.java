b5e0e33ef4bd073d28bbff4eb07fecba48f1c284
Poxman100 <65652333+Poxman100@users.noreply.github.com>
2025-09-14 14:07:26 +0800

    Add javadoc for Deadline class

--- src/main/java/pecky/Deadline.java
+++ src/main/java/pecky/Deadline.java
@@ -2,6 +2,11 @@ package pecky;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a deadline. A <code>Deadline</code> object has a description
+ * and a date e.g., <code>Chemistry homework, 3rd August 2025 2359.</code>
+ */
+
 public class Deadline extends Task {
 
     protected LocalDateTime by;
@@ -10,6 +15,15 @@ public class Deadline extends Task {
         super(description);
         this.by = by;
     }
+
+    /**
+     * Returns a new Deadline object.
+     * If the string pattern of the date and time is invalid, a null object is returned.
+     *
+     * @param description Description of the deadline.
+     * @param by The date and time of the deadline.
+     * @return A new deadline object.
+     */
 
     public static Deadline createDeadline(String description, String by) {
         LocalDateTime byDate = convertStringToDate(by);
@@ -20,17 +34,46 @@ public class Deadline extends Task {
         return new Deadline(description, byDate);
     }
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method returns a string representation of the Deadline object
+     * in the format "[D] [status] description (by: date)".
+     * This output is useful to print to the end-user, and for debugging purposes.
+     *
+     * @return String representation of Deadline object.
+     */
+
     @Override
     public String toString() {
         String formattedDate = this.by.format(TO_STRING_FORMATTER);
         return "[D]" + super.toString() + " (by: " + formattedDate + ")";
     }
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method returns a string representation of the Deadline object
+     * in the format "D|isDone|description|date".
+     * This output is useful for reading and writing to a text file.
+     *
+     * @return String representation of Deadline object.
+     */
+
     @Override
     public String toTaskListString() {
         String formattedDate = this.by.format(TO_TASK_LIST_STRING_FORMATTER);
         return "D|" + (this.isDone ? 1 : 0) + "|" + this.description + "|" + formattedDate;
     }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method returns true if the deadline is occurring within 0 to 24 hours after the
+     * provided dateTime, and false otherwise.
+     *
+     * @return True if the deadline is occurring within 24 hours of dateTime, and false otherwise.
+     */
 
     @Override
     public boolean onDay(LocalDateTime dateTime) {
