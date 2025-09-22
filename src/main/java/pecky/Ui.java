e576ead4e0024b1c6485ff94f0cb8eb2fec5883a
Poxman100 <65652333+Poxman100@users.noreply.github.com>
2025-09-22 20:50:14 +0800

    Resolve issue where commandList.txt cannot be read

--- src/main/java/pecky/Ui.java
+++ src/main/java/pecky/Ui.java
@@ -1,9 +1,9 @@
 package pecky;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
@@ -19,9 +19,9 @@ public class Ui {
             + "What can I do for you?";
     private static final String BYE = "Bye. Hope to see you again soon!";
     private static final String UNKNOWN = "OOPS!!! I'm sorry, but I don't know what that means :-(";
-    private static final Path commandListFile = Paths.get("./data/commandList.txt");
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String HELP = initializeHelp();
 
     public static void setMainWindow(MainWindow mainWindow) {
         assert mainWindow != null;
@@ -72,18 +72,39 @@ public class Ui {
         Ui.print(UNKNOWN);
     }
 
+
+
+    private static String initializeHelp() {
+        // The below was created with the help of google AI.
+
+        InputStream inputStream = Ui.class.getClassLoader().getResourceAsStream("commandList.txt");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
     /**
      * Helps the user by printing the command list if an unknown input
      * is encountered 3 times.
      */
 
     public static void help() {
-        try {
-            Ui.print(Files.readString(commandListFile));
-        } catch (IOException e) {
-            Ui.print("You entered 3 incorrect commands."
+        if (!HELP.isEmpty()) {
+            Ui.print(HELP);
+            return;
+        }
+
+        Ui.print("You entered 3 incorrect commands."
                     + "\nUnfortunately I lost my command list T_T"
                     + "\nCan't help you :(");
-        }
     }
 }
